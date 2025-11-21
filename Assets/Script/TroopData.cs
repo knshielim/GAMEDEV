61dd8d4dc6ddd7cf017329cc077ced59b74e7cde
Noname <beatriceaurelia13@gmail.com>
2025-11-21 16:00:56 +0700

    Add troop config(1)

---
+++ Assets/Script/TroopData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TroopRarity
+{
+    Common,
+    Rare,
+    Epic,
+    Legendary,
+    Mythic
+}
+
+[CreateAssetMenu(
+    fileName = "NewTroopData",
+    menuName = "Game/Troop Data"
+)]
+public class TroopData : ScriptableObject
+{
+    [Header("Basic Info")]
+    public string id;              // misal "slime_blue"
+    public string displayName;     // misal "Blue Slime"
+    public TroopRarity rarity;
+
+    [Header("Stats")]
+    public int maxHealth;
+    public int attack;
+    public float moveSpeed;        // jalan ke depan seberapa cepat
+    public float attackInterval;   // waktu antar serangan (detik)
+
+    [Header("Prefab Reference")]
+    public GameObject prefab;      // prefab unit (Slime_Blue, Warrior_1, dst)
+}
+
